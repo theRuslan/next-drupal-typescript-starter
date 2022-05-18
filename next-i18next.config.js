166341602dd4d8f2a53f7649854686802610f036
theRuslan <skiffle.rus@gmail.com>
2022-05-18 20:22:19 +0500

    added next-i18next config

---
+++ next-i18next.config.js
@@ -0,0 +1,16 @@
+const path = require("path");
+
+module.exports = {
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en", "ru"],
+    localeDetection: false,
+    debug: false,
+  },
+  localePath: path.resolve("locales"),
+  /*
+   * Reloading Resources in Development:
+   * https://github.com/isaachinman/next-i18next#reloading-resources-in-development
+   */
+  reloadOnPrerender: true,
+};
