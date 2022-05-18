9aebd35d1ba8111cd6e4044cc3f16ca960ea14aa
theRuslan <skiffle.rus@gmail.com>
2022-05-18 21:24:02 +0500

    added DrupalClient config

---
+++ lib/drupal.ts
@@ -0,0 +1,16 @@
+import { Experiment_DrupalClient as DrupalClient } from "next-drupal";
+
+export const drupal = new DrupalClient(
+  process.env.NEXT_PUBLIC_DRUPAL_BASE_URL,
+  {
+    debug: false,
+    apiPrefix: "/jsonapi",
+    frontPage: process.env.DRUPAL_FRONT_PAGE,
+    previewSecret: process.env.DRUPAL_PREVIEW_SECRET,
+    withAuth: true,
+    auth: {
+      clientId: process.env.DRUPAL_CLIENT_ID,
+      clientSecret: process.env.DRUPAL_CLIENT_SECRET,
+    },
+  }
+);
