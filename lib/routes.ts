aeab356e159d7d56b297a519bad6c9bc0401199a
theRuslan <skiffle.rus@gmail.com>
2022-05-18 21:24:25 +0500

    added routes file

---
+++ lib/routes.ts
@@ -0,0 +1,14 @@
+const routes = {
+  home: "/",
+
+  // paths can be functions
+  // this also makes it easier to change from "id" to "slug" in the future
+  // product(pid: number) {
+  //   return {
+  //     href: `/products/${pid}`,
+  //     as: `/products/${pid}`,
+  //   };
+  // },
+};
+
+export default routes;
