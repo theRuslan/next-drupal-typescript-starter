9508c12da2592263b54cbfe3509f82572815ac5d
theRuslan <skiffle.rus@gmail.com>
2022-05-18 20:23:40 +0500

    updated ts-related files

---
+++ environment.d.ts
@@ -0,0 +1,20 @@
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      NODE_ENV: "development" | "production" | "test";
+      NEXT_PUBLIC_DRUPAL_BASE_URL: string;
+      NEXT_IMAGE_DOMAIN: string;
+      DRUPAL_SITE_ID: string;
+      DRUPAL_FRONT_PAGE: string;
+      DRUPAL_PREVIEW_SECRET: string;
+      DRUPAL_CLIENT_ID: string;
+      DRUPAL_CLIENT_SECRET: string;
+      NEXT_PUBLIC_GA_ID: string;
+      NEXT_PUBLIC_YM_ID: number;
+    }
+  }
+}
+
+// If this file has no import/export statements (i.e. is a script)
+// convert it into a module by adding an empty export statement.
+export {};
