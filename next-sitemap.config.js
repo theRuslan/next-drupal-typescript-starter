f85ff558ba8b38bc80f381cd3b7795aaba2e963f
theRuslan <skiffle.rus@gmail.com>
2022-06-05 20:39:46 +0500

    next-sitemap update

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 
 module.exports = {
-  siteUrl: process.env.SITE_URL || "https://domain.com",
+  siteUrl: process.env.SITE_URL || "https://starter.wakelabstudio.ru",
   changefreq: "daily",
   priority: 0.7,
   sitemapSize: 5000,
