34885a696c8b15fa3d7d4ae6fd76d886dbd53357
theRuslan <skiffle.rus@gmail.com>
2022-06-05 20:40:09 +0500

    next-i18next config update

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -5,12 +5,12 @@ module.exports = {
     defaultLocale: "en",
     locales: ["en", "ru"],
     localeDetection: false,
-    debug: false,
+    debug: process.env.NODE_ENV !== "production",
   },
   localePath: path.resolve("locales"),
   /*
    * Reloading Resources in Development:
    * https://github.com/isaachinman/next-i18next#reloading-resources-in-development
    */
-  reloadOnPrerender: true,
+  reloadOnPrerender: process.env.NODE_ENV !== "production",
 };
