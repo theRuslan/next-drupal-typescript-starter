d2b9cb7bc1108ea68878717c7bbd72777572b3df
theRuslan <skiffle.rus@gmail.com>
2022-06-06 03:17:52 +0500

    app updated

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,39 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+import { appWithTranslation } from "next-i18next";
+import getConfig from "next/config";
+import React from "react";
+import TagManager from "react-gtm-module";
 
-export default MyApp
+import type { AppProps } from "next/app";
+
+const App = ({ Component, pageProps }: AppProps) => {
+  const { publicRuntimeConfig } = getConfig();
+
+  // Initializing Google Tag Manager once on load with checking for production
+  // environment.
+  React.useEffect(() => {
+    const tagManagerArgs = {
+      gtmId: publicRuntimeConfig.gtmId,
+    };
+
+    if (process.env.NODE_ENV === "production") {
+      TagManager.initialize(tagManagerArgs);
+    }
+  }, [publicRuntimeConfig.gtmId]);
+
+  return (
+    <>
+      <Component {...pageProps} />
+    </>
+  );
+};
+
+// https://nextjs.org/docs/advanced-features/measuring-performance
+// export function reportWebVitals(metric) {
+//   console.log(metric)
+// }
+
+// export default appWithTranslation(App);
+
+export default appWithTranslation(App);
