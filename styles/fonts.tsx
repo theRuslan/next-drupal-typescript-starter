22ddaa85f54e9e3acf9197f5c3b87c591df90190
theRuslan <skiffle.rus@gmail.com>
2022-06-09 22:46:26 +0500

    added template for custom self-hosted font loading

---
+++ styles/fonts.tsx
@@ -0,0 +1,31 @@
+import { Global } from "@emotion/react";
+
+const Fonts = () => (
+  <Global
+    styles={`
+        @font-face {
+          font-family: "AkzidenzGroteskPro";
+          font-style: normal;
+          font-weight: 300;
+          font-display: swap;
+          src: url(/fonts/akzidenz-grotesk/AkzidenzGroteskPro-LightEx.woff)
+              format("woff"),
+            url(/fonts/akzidenz-grotesk/AkzidenzGroteskPro-LightEx.ttf)
+              format("truetype");
+        }
+
+        @font-face {
+          font-family: "AkzidenzGroteskPro";
+          font-style: normal;
+          font-weight: 500;
+          font-display: swap;
+          src: url(/fonts/akzidenz-grotesk/AkzidenzGroteskPro-MdEx.woff)
+              format("woff"),
+            url(/fonts/akzidenz-grotesk/AkzidenzGroteskPro-MdEx.ttf)
+              format("truetype");
+        }
+      `}
+  />
+);
+
+export default Fonts;
