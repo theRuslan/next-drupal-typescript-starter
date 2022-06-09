94696bceefaf902cd68115f0c9b8692d9f97122c
theRuslan <skiffle.rus@gmail.com>
2022-06-09 22:58:19 +0500

    removed default vercel logo in footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -98,19 +98,6 @@ const Home: NextPage = () => {
           <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
         </Container>
       </Wrapper>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <span>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </>
   );
 };
