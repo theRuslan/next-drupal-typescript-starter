0472322bbcb83ddc6ca1354a35977ba2a4ed0631
theRuslan <skiffle.rus@gmail.com>
2022-06-09 22:47:16 +0500

    added routes for example pages

--- lib/routes.ts
+++ lib/routes.ts
@@ -1,5 +1,9 @@
 const routes = {
   home: "/",
+  examplePageSSG: "/example-page-SSG",
+  examplePageSSR: "/example-page-SSR",
+  examplePageISR: "/example-page-ISR",
+  exampleSearch: "/example-search",
 
   // paths can be functions
   // this also makes it easier to change from "id" to "slug" in the future
